d11f0e6be0455ef2108289dfc47b06f55e83ea2a
Jonah Unuafe <fejisfejiro@gmail.com>
2024-01-27 21:02:41 +0100

    a simple login page

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import AuthInputs from './components/AuthInputs.jsx';
+import Header from './components/Header.jsx';
+import Button from './components/Button.jsx';
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <AuthInputs />
+        <Button />
+      </main>
+    </>
+  );
+}
